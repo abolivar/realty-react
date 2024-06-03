9bb9a436e1499b130d7eda9be637f7640438d8bc
Argenis David Bolivar <abolivar@gmail.com>
2024-06-03 15:11:33 -0500

    pages/routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
+import ForgotPassword from "./pages/ForgotPassword";
+import Home from "./pages/Home";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <>
-      <h1 className='text-2x1 bg-red-400'>
-        Hola mundo
-      </h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/forgot-passsword" element={<ForgotPassword />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+
+
+
+        </Routes>
+      </Router>
     </>
   );
 }
